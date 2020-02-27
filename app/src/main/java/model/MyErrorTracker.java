f12d17289e0de0091aa37d096b2f44f9bfc627b5
theblo <isaac.afrifa3@yahoo.com>
2020-02-27 23:25:41 +0000

    built the apk

--- app/src/main/java/model/MyErrorTracker.java
+++ app/src/main/java/model/MyErrorTracker.java
@@ -9,7 +9,7 @@ Main Responsibility : HELP US TRACK CONNECTION EXCEPTIONS AT RUNTIME
     public final static String CONNECTION_ERROR="Error : Unable To Establish Connection ";
     //Changed cos the error is mostly Internet Connectivity
     // public final static String IO_EROR="Error : Unable To Read ";
-    public final static String IO_EROR="Error : Little or No Internet Connectivity ";
+    public final static String IO_EROR="Error : Poor Internet Connectivity ";
     public final static String RESPONSE_EROR="Error : Bad Response - ";
     public final static String EMPTY_URL="Error : No URL Found ";
     public final static String PASS_ERROR="Error: Unable To Parse";
